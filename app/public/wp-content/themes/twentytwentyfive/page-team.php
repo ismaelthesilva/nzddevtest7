f491dfae726417837ce0ebcf2cb188d2f6c0432d
Ismael Silva <ismaelsilva@icloud.com>
2025-11-03 15:11:33 +1300

    Final deployment

--- app/public/wp-content/themes/twentytwentyfive/page-team.php
+++ app/public/wp-content/themes/twentytwentyfive/page-team.php
@@ -1,20 +1,5 @@
-<?php
-/*
-Template Name: Team Page
-*/
-get_header();
-?>
-<main class="site-main">
-  <div class="container">
-    <?php
-    // Page content (optional)
-    if ( have_posts() ) :
-      while ( have_posts() ) : the_post();
-        the_content();
-      endwhile;
-    endif;
-    ?>
-    <?php include( get_template_directory() . '/parts/team-section.php' ); ?>
-  </div>
-</main>
-<?php get_footer(); ?>
+<section class="team-section">
+  <h2>Our People</h2>
+  
+  [my_team_grid]
+</section>
